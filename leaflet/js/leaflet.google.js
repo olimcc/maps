6752fe300027f1bd1a0273722a6f7298b6774a6e
Oli Mcc <dev@olimcc.com>
2012-04-23 00:39:59 -0700

    adding demo using leaflet js api

---
+++ leaflet/js/leaflet.google.js
@@ -0,0 +1,38 @@
+function addPoly(map) {
+  var triangleCoords = [
+    new L.LatLng(54.774252, -8.190262),
+    new L.LatLng(54.466465, -8.118292),
+    new L.LatLng(54.321384, -8.75737)
+  ];
+
+  var initOptions = {color: 'blue'}
+  var hoverOpts = {color: 'red'};
+
+  tri = new L.Polygon(triangleCoords);
+
+  // Add a listener for events
+  tri.on('mouseover', function() {
+    tri.setStyle(hoverOpts);
+  });
+  tri.on('mouseout', function() {
+    tri.setStyle(initOptions);
+  });
+  tri.on('click', function() {
+    alert('clicked');
+  });
+
+  // are there bad implications for adding this as a layer?
+  map.addLayer(tri);
+}
+
+function initialize() {
+  var map = new L.Map('map_canvas');
+  // using api key from leaflet for demo only
+  var cloudmadeLayer = new L.TileLayer('http://mt1.google.com/vt/lyrs=m@176000000&hl=en&src=api&x={x}&y={y}&z={z}&s=Galil', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+});
+  var center = new L.LatLng(54.265224,-8.530884);
+  map.setView(center, 8).addLayer(cloudmadeLayer);
+  addPoly(map);
+}
